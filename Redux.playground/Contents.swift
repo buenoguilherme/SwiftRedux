785effb21493dd230c7d40ec81f668ed14477dae
Guilherme Bueno <guilherme.bueno@guiabolso.com.br>
2019-03-30 20:08:55 -0300

    Initial commit with basic implementation

---
+++ Redux.playground/Contents.swift
@@ -0,0 +1,81 @@
+import Foundation
+
+typealias State = [String: Any]
+
+enum ReduxError: Error {
+    case typeError
+}
+
+enum Action {
+    case add(number: Int)
+    case minus(number: Int)
+}
+
+protocol ReducerType {
+    associatedtype StateType
+
+    var stateName: String { get }
+    var initialValue: StateType { get }
+    var f: (StateType, Action) throws -> StateType { get }
+}
+
+struct Reducer: ReducerType {
+    typealias StateType = Any
+
+    let stateName: String
+    let initialValue: Any
+    let f: (Any, Action) throws -> Any
+}
+
+let currentValue = Reducer(stateName: "currentValue", initialValue: 0) { state, action in
+    print("dentro do reducer 1: \(state)")
+    guard let state = state as? Int else {
+        throw ReduxError.typeError
+    }
+
+    print("dentro do reducer 2: \(state)")
+
+    switch action {
+    case .add(let number):
+        return state + number
+    default:
+        return state
+    }
+}
+
+func add(_ number: Int) -> Action {
+    return .add(number: number)
+}
+
+
+class Store {
+    private var state: State = State()
+    private let reducers: [Reducer]
+
+    init(reducers: [Reducer]) {
+        self.reducers = reducers
+    }
+
+    func dispatch(action: Action) throws {
+        try reducers.forEach { reducer in
+            let reducerState = state[reducer.stateName] ?? reducer.initialValue
+            state[reducer.stateName] = try reducer.f(reducerState, action)
+        }
+    }
+
+    func getState() -> State {
+        return state
+    }
+}
+
+let store = Store(reducers: [currentValue])
+print(store.getState())
+
+do {
+    try store.dispatch(action: add(1))
+    print(store.getState())
+} catch {
+    print("Deu erro!")
+}
+
+
---
+++ SwiftRedux/SwiftRedux/SwiftRedux.swift
@@ -0,0 +1,13 @@
+//
+//  SwiftRedux.swift
+//  SwiftRedux
+//
+//  Created by Guilherme Silva on 30/03/19.
+//  Copyright © 2019 Guilherme Bueno. All rights reserved.
+//
+
+import Foundation
+
+class SwiftRedux {
+
+}
